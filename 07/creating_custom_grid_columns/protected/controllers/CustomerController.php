fa7cb37c302b7d298ea40650c2332695a73aa265
Carsten Brandt <mail@cebe.cc>
2012-12-19 21:38:02 +0100

    removed unused code

--- 07/creating_custom_grid_columns/protected/controllers/CustomerController.php
+++ 07/creating_custom_grid_columns/protected/controllers/CustomerController.php
@@ -8,50 +8,14 @@ class CustomerController extends CController
 	 */
 	public $layout='//layouts/column2';
 
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
+	public function actionFlag($pk, $name, $value)
 	{
-		return array(
-			//'accessControl', // perform access control for CRUD operations
-		);
-	}
-
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
-	}
-
-	public function actionFlag($pk, $name, $value){
 		$model = $this->loadModel($pk);
 		$model->{$name} = $value;
 		$model->save(false);
 
-		if(!Yii::app()->request->isAjaxRequest){
+		if(!Yii::app()->request->isAjaxRequest)
 			$this->redirect('admin');
-		}
 	}
 
 	/**
