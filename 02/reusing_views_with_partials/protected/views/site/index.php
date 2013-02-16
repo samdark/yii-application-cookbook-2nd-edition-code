2a3d5517ac2146aabe63669d378dfdcea34c6321
Alexander Makarov <sam@rmcreative.ru>
2013-02-16 04:00:21 +0400

    added code for reusing views with partials

---
+++ 02/reusing_views_with_partials/protected/views/site/index.php
@@ -0,0 +1,5 @@
+<?php $this->renderPartial('//common/youtube', array(
+	'id' => '8Rp-CaIKvQs', // you can get this id by simply looking at video URL
+	'width' => 320,
+	'height' => 256,
+))?>
---
+++ 02/reusing_views_with_partials/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+	`id` int(10) unsigned NOT NULL auto_increment,
+	`name` varchar(200) NOT NULL,
+	`email` varchar(200) NOT NULL,
+	PRIMARY KEY  (`id`)
+);
